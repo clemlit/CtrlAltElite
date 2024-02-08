379733c65a9348531cbce01d22ce2da47afcbc5e
Wojtecki Benjamin <benjamin.wojtecki@etudiant.univ-rennes1>
2024-02-08 18:20:52 +0100

    modification de App sachant que triagne ne marche pas (enSVG)

--- src/App.java
+++ src/App.java
@@ -1,14 +1,30 @@
+import fr.univrennes.istic.l2gen.geometrie.Cercle;
 import fr.univrennes.istic.l2gen.geometrie.Ellipse;
 import fr.univrennes.istic.l2gen.geometrie.Groupe;
 import fr.univrennes.istic.l2gen.geometrie.IForme;
+import fr.univrennes.istic.l2gen.geometrie.Ligne;
+import fr.univrennes.istic.l2gen.geometrie.Polygone;
+import fr.univrennes.istic.l2gen.geometrie.Rectangle;
+import fr.univrennes.istic.l2gen.geometrie.Secteur;
+import fr.univrennes.istic.l2gen.geometrie.Triangle;
+import java.io.FileWriter;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        IForme f = new Ellipse (256 , 256 , 128 , 64);
-        System.out.println(f.enSVG ());
-        Groupe arbre = arbre(f);
-        System.out.println(arbre.enSVG());
+        Groupe tableau = new Groupe();
+
+        tableau.ajoutGroupe(new Triangle(192 , 128 , 256 , 128 , 256 , 256l));
+
+        System.out.println(tableau.enSVG());
+
+        String svgContent = "<svg xmlns=\"http://www.w3.org/2000/svg\">\n" +tableau.enSVG() +"</svg>";
+        FileWriter writer = new FileWriter("figure.svg");
+
+        writer.write(svgContent);
+        writer.close();
+
+        System.out.println("Le fichier Cercle.svg a été créé avec succès.");
     }
 
     // Méthode pour créer un arbre avec des groupes et des ellipses

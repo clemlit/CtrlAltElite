70620502d60dcb305b6bf04b4f1b7744cd829330
Chaomare <not.chaomare@gmail.com>
2024-02-08 17:44:57 +0100

    ajout javaDoc Polygone

--- src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
@@ -13,29 +13,42 @@ public class Polygone implements IForme {
             liste.add(p);
         }
     }
-
+    /**
+     * @return 0 car pas de hauteur que l'on peut définir pour un polygone quelconque
+     */
     public double hauteur(){
         return 0;
     }
-
+    /**
+     * @return le point 0,0 car pas de centre que l'on peut définir pour un polygone quelconque
+     */
     public Point centre(){
         Point p = new Point(0, 0);
         return p;
     }
 
+    /**
+     * @return 0 car pas de largeur que l'on peut définir pour un polygone quelconque
+     */
     public double largeur(){
         return 0;
     }
-
+    /**
+     * @return rien car jsp comment redimensionner un polygone quelconque
+     */
     public void redimensionner(double dx, double dy){
 
     }
-
+    /**
+     * @return rien car jsp comment déplacer un polygone quelconque
+     */
     public void deplacer(double dx, double dy){
 
     }
 
-    
+    /**
+     * @return Une liste de tous les sommets
+     */
     public ArrayList<Point> getSommets() {
         ArrayList<Point> list = new ArrayList<Point>();
         for (int i = 0; i < liste.size(); i++) {
@@ -43,7 +56,10 @@ public class Polygone implements IForme {
         }
         return list;
     }
-    
+    /**
+     * @param i entre les points
+     * @return un String décrivant les coordonnées de tous les points du polygone
+     */
     @Override
     public String description(int i) {
         String txt = "Polygone";
@@ -55,15 +71,28 @@ public class Polygone implements IForme {
         }
         return txt;
     }
+
+    /**
+     * @param p point quelconque avec coordonnées quelconques
+     * @return ajoute un point au polygone
+     */
     public void ajouterSommet(Point p) {
         liste.add(p);
     }
     
+    /**
+     * @param x coordonnée x du point que l'on veut ajouter
+     * @param x coordonnée y du point que l'on veut ajouter
+     * @return ajoute le point avec les coordonnées correspondantes au polygone
+     */
     public void ajouterSommet(double x, double y) {
         Point p = new Point(x, y);
         liste.add(p);
     }
     
+    /**
+     * @return le fichier SVG créant le polygone
+     */
     @Override
     public String enSVG() {
 

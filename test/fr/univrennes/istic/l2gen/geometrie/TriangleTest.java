c48e4599f2b8a6d098582293e7b03c7f2dc54873
maelecoq <mael.le-coq@etudiant.univ-rennes1.fr>
2024-02-15 15:52:50 +0100

    ajout TriangleTest

---
+++ test/fr/univrennes/istic/l2gen/geometrie/TriangleTest.java
@@ -0,0 +1,20 @@
+package test.fr.univrennes.istic.l2gen.geometrie;
+
+import org.junit.Test;
+
+public class TriangleTest {
+    @Test
+    public void testDescription() {
+
+    }
+
+    @Test
+    public void testDupliquer() {
+
+    }
+
+    @Test
+    public void testEnSVG() {
+
+    }
+}

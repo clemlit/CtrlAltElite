26f0a7488579c696109bd3ae27ea19a9cefe2b33
maelecoq <mael.le-coq@etudiant.univ-rennes1.fr>
2024-02-15 18:19:03 +0100

    modifications de la classe Triangle et ajout TriangleTest

--- test/fr/univrennes/istic/l2gen/geometrie/TriangleTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/TriangleTest.java
@@ -1,11 +1,30 @@
 package test.fr.univrennes.istic.l2gen.geometrie;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
 
+import fr.univrennes.istic.l2gen.geometrie.Cercle;
+import fr.univrennes.istic.l2gen.geometrie.IForme;
+import fr.univrennes.istic.l2gen.geometrie.Point;
+import fr.univrennes.istic.l2gen.geometrie.Triangle;
+
 public class TriangleTest {
+
     @Test
     public void testDescription() {
+        int indentation = 4;
+        IForme triangle = new Triangle(120.0, 66.0, 69.0);
+        Point centre = cercle.centre();
+        String ind = "";
+        for (int i = 0; i < indentation; i++) {
+            ind += "  ";
+        }
+        ind = ind + "Cercle centre = " + centre.x() + " , " + centre.y() + " r = " + cercle.hauteur();
 
+        assertEquals(ind, cercle.description(4));
+        assertNotEquals(ind, cercle.description(8));
     }
 
     @Test
@@ -15,6 +34,56 @@ public class TriangleTest {
 
     @Test
     public void testEnSVG() {
+
+    }
+
+    @Test
+    public void testCentre() {
+
+    }
+
+    @Test
+    public void testColorier() {
+
+    }
+
+    @Test
+    public void testDeplacer() {
+
+    }
+
+    @Test
+    public void testDescription2() {
+
+    }
+
+    @Test
+    public void testDupliquer2() {
+
+    }
+
+    @Test
+    public void testEnSVG2() {
+
+    }
+
+    @Test
+    public void testGetCouleur() {
+
+    }
+
+    @Test
+    public void testHauteur() {
+
+    }
+
+    @Test
+    public void testLargeur() {
+
+    }
+
+    @Test
+    public void testRedimensionner() {
 
     }
 }

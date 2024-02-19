75331bf8f59356c0aa7abd0ce931fe50692fe371
Wojtecki Benjamin <benjamin.wojtecki@etudiant.univ-rennes1>
2024-02-20 00:10:32 +0100

    Ajout de la méthode aligner

--- src/fr/univrennes/istic/l2gen/geometrie/Ellipse.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Ellipse.java
@@ -173,5 +173,31 @@ public class Ellipse implements IForme {
     public int getAngle() {
         return this.angle;
     }
+
+    /**
+     * Aligne le cercle en fonction de l'alignement spécifié et de la cible donnée.
+     *
+     * @param alignment Alignement souhaité (HAUT, BAS, GAUCHE, DROITE).
+     * @param cible     Coordonnée cible pour l'alignement.
+     * @return Instance de la forme géométrique alignée.
+     */
+    @Override
+    public IForme aligner(Alignement alignment, double cible) {
+        switch (alignment) {
+            case HAUT:
+                this.y = cible + this.demiPetitAxe;
+                break;
+            case BAS:
+                this.y = cible - this.demiPetitAxe;
+                break;
+            case GAUCHE:
+                this.x = cible + this.demiGrandAxe;
+                break;
+            case DROITE:
+                this.x = cible - this.demiGrandAxe;
+                break;
+        }
+        return this;
+    }
     
 }

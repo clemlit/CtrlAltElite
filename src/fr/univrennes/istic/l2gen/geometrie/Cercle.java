9555a0a5cf6711c744f7674e45c67b02d85a934c
Wojtecki Benjamin <benjamin.wojtecki@etudiant.univ-rennes1>
2024-02-23 00:22:18 +0100

    Modification dans le enSVG pour faire des retours à la ligne

--- src/fr/univrennes/istic/l2gen/geometrie/Cercle.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Cercle.java
@@ -182,9 +182,7 @@ public class Cercle implements IForme {
     @Override
     public IForme colorier(String... couleurs) {
         if (couleurs.length > 0) {
-            // Ici, vous pouvez prendre la première couleur du tableau couleurs
             String couleur = couleurs[0];
-            // Implémentation pour colorier un cercle avec la couleur spécifiée
             this.couleur = couleur;
         }
         return this;

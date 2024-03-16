34ff7bf7fc473b3b1422b2684aca87f858ee5579
Wojtecki Benjamin <benjamin.wojtecki@etudiant.univ-rennes1>
2024-03-16 20:13:37 +0100

    Ajout des différentes méthodes ainsi que gestion de l'affichage

--- src/fr/univrennes/istic/l2gen/visustats/DiagCamemberts.java
+++ src/fr/univrennes/istic/l2gen/visustats/DiagCamemberts.java
@@ -7,6 +7,7 @@ import java.util.List;
 import fr.univrennes.istic.l2gen.geometrie.IForme;
 import fr.univrennes.istic.l2gen.geometrie.Point;
 import fr.univrennes.istic.l2gen.geometrie.Rectangle;
+import fr.univrennes.istic.l2gen.geometrie.Texte;
 
 
 
@@ -15,10 +16,20 @@ public class DiagCamemberts implements IDataVisualiseur {
     private Camembert camembert;
     private String couleur; 
     private String legence;
+    private String titre;
     private List<Rectangle> Rec;
     private int nbreDePart;
     private List<Camembert> camemberts;
+    private List<Texte> LegendeTexte;
     private StringBuilder legendeSVG = new StringBuilder();
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 
     public String getLegence() {
         return legence;
@@ -37,42 +48,62 @@ public class DiagCamemberts implements IDataVisualiseur {
     }
 
     public DiagCamemberts(String Titre, int x) {
-        this.legence = Titre;
+        this.titre = Titre;
         this.nbreDePart = x;
         this.camemberts = new ArrayList<>();
         this.Rec = new ArrayList<>();
-
+        this.LegendeTexte = new ArrayList<>(); 
     }
 
     @Override
     public IDataVisualiseur agencer() {
         if (camemberts != null) {
-            int xOffset = 100; 
-            for (Camembert currentCamembert : camemberts) {
+            int xOffset = 200; 
+            int longeurtotale = (int)camemberts.get(0).centre().x();
+            int longueurTexte = getTitre().length();
+            for (int i = 0; i < camemberts.size(); i++) {
+                Camembert currentCamembert = camemberts.get(i);
                 currentCamembert.deplacer(xOffset, 0);
-                xOffset += currentCamembert.largeur() + 20; 
+                Texte legende = LegendeTexte.get(i);
+                legende.deplacer(xOffset, 0);
+                xOffset += currentCamembert.largeur() + 70;
             }
+
+            // Calculer la largeur totale des camemberts
+            for (Camembert camembert : camemberts) {
+                longeurtotale += camembert.centre().x();
+            }
+            int centerX = longeurtotale /  camemberts.size();
+            Texte texteTitre = new Texte(centerX - longueurTexte * 3, 50, 20, getTitre());
+            this.legendeSVG.append(texteTitre.enSVG());
         }
         return this;
     }
 
-
     @Override
     public IDataVisualiseur ajouterDonnees(String legende, double... valeurs) {
-        if (valeurs.length > 0) {
-            double total = 0;
-            for (double valeur : valeurs) {
-                total += valeur;
-            }
-        
-            for (int i = 0; i < valeurs.length; i++) {
-                String couleur = "Couleur" + i;
-                double pourcentage = valeurs[i] / total;
-                camembert.ajouterSecteur(couleur, pourcentage);
-            }
+        Camembert nouveauCamembert = new Camembert(250, 250, 100);
+
+        double total = 0;
+        for (double valeur : valeurs) {
+            total += valeur;
         }
 
-        return this; 
+        // Ajouter les secteurs au nouveau camembert
+        for (double valeur : valeurs) {
+            double pourcentage = valeur / total;
+            nouveauCamembert.ajouterSecteur(couleur, pourcentage);
+        }
+
+        // Ajouter le nouveau camembert à la liste des camemberts
+        camemberts.add(nouveauCamembert);
+
+        // Créer un rectangle sous le camembert pour la légende
+        Texte texte = new Texte((int) nouveauCamembert.getCentre().x(),
+                (int) (nouveauCamembert.getCentre().y() + nouveauCamembert.getRayon() + 20), 15, legende);
+        LegendeTexte.add(texte);
+
+        return this;
     }
 
     @Override
@@ -88,8 +119,8 @@ public class DiagCamemberts implements IDataVisualiseur {
 
         if (legendes.length > 0) {
             // Déterminons la position de départ de la légende
-            int startX = (int) camembert.centre().x();
-            int startY = (int) (camembert.hauteur() + 180);
+            int startX = (int) camembert.centre().x() + 400;
+            int startY = (int) (camembert.hauteur() + 300);
 
             // Ajoutons la légende pour chaque carré de couleur
             for (int i = 0; i < legendes.length; i++) {
@@ -109,7 +140,6 @@ public class DiagCamemberts implements IDataVisualiseur {
             }
         }
         return this;
-
     }
 
     @Override
@@ -117,7 +147,10 @@ public class DiagCamemberts implements IDataVisualiseur {
         if (camembert == null) {
             camembert = new Camembert(250, 250, 100);
         }
-        camembert.colorier(couleurs);
+        
+        for (Camembert camembert : camemberts){
+            camembert.colorier(couleurs);
+        }
 
         //ATTENTION DIVISION PAR 0
         if (couleurs.length == 0) {
@@ -134,33 +167,24 @@ public class DiagCamemberts implements IDataVisualiseur {
         return this;
     }
 
-
-    @Override
-    public IForme deplacer(double x, double y) {
-        return camembert.deplacer(x, y);
-    }
-
-    @Override
-    public String description(int indentation) {
-        return camembert.description(indentation);
-    }
-
-    @Override
-    public IForme dupliquer() {
-        return camembert.dupliquer();
-    }
-
     @Override
     public String createEnSVG() {
-        if (camembert == null) {
-            camembert = new Camembert(250, 250, 100); 
+        String svg = "";
+        // Générer le SVG de chaque camembert
+        for (Camembert currentCamembert : camemberts) {
+            svg += currentCamembert.enSVG();
         }
 
-        String updatedCamembertSVG = camembert.createEnSVG();
+        for (Texte legende : LegendeTexte ){
+            svg += legende.enSVG();
+        }
+        svg += legendeSVG;
+        return svg;
+    }
 
-        String resultSVG = updatedCamembertSVG + camembert.getLegende();
-
-        return "<svg xmlns=\"http://www.w3.org/2000/svg\">" + resultSVG + "</svg>";
+    @Override
+    public String enSVG() {
+        return "<svg xmlns=\"http://www.w3.org/2000/svg\">" + createEnSVG() + "</svg>";
     }
     
 
@@ -192,16 +216,21 @@ public class DiagCamemberts implements IDataVisualiseur {
     @Override
     public String getCouleur() {
         return couleur;
+    }    
+    
+    @Override
+    public IForme deplacer(double x, double y) {
+        return camembert.deplacer(x, y);
     }
 
     @Override
-    public String enSVG() {
-
-        String updatedCamembertSVG = camembert.createEnSVG();
-        String resultSVG = updatedCamembertSVG + legendeSVG.toString();
-
-        return "<svg xmlns=\"http://www.w3.org/2000/svg\">" + resultSVG + "</svg>";
-    
+    public String description(int indentation) {
+        return camembert.description(indentation);
     }
-    
+
+    @Override
+    public IForme dupliquer() {
+        return camembert.dupliquer();
+    }
+
 }

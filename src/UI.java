7644d54791e98e923d20e7b300ea1f18903401c3
Messager Clément <clement.messager@etudiant.univ-rennes1.fr>
2024-03-19 16:35:59 +0100

    UI choix échelle

--- src/UI.java
+++ src/UI.java
@@ -14,10 +14,22 @@ public class UI {
 
     String[] Carburant = { "Gazole", "SP95", "SP98", "E10","E85", "GPLc"}; 
     JComboBox jCombo = new JComboBox(Carburant);
+
     pannelCarburant.add(jCombo);
+    JPanel pannelechelle = new JPanel();
+    Border border2 = BorderFactory.createTitledBorder("échelle");
+    pannelechelle.setBorder(border2);
+    ButtonGroup group = new ButtonGroup();
+    JRadioButton radio1 = new JRadioButton("Région", true);
+    JRadioButton radio2 = new JRadioButton("Département");
+    group.add(radio1);
+    pannelechelle.add(radio1);
+    group.add(radio2);
+    pannelechelle.add(radio2);
+
 
     f.add(pannelCarburant);
-    
+    f.add(pannelechelle,BorderLayout.NORTH);
     f.setVisible(true);
   }
 }

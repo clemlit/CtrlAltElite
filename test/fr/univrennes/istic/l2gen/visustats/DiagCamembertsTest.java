15feb122af05a9ff0e0bc5fd3af6baa29ef94c1e
Sami Saoud <sami.saoud@etudiant.univ-rennes1.fr>
2024-03-21 16:47:16 +0100

    Ajouts des tests (correction)

---
+++ test/fr/univrennes/istic/l2gen/visustats/DiagCamembertsTest.java
@@ -0,0 +1,115 @@
+package test.fr.univrennes.istic.l2gen.visustats;
+
+import org.junit.Test;
+
+public class DiagCamembertsTest {
+    @Test
+    public void testAgencer() {
+
+    }
+
+    @Test
+    public void testAjouterDonnees() {
+
+    }
+
+    @Test
+    public void testCentre() {
+
+    }
+
+    @Test
+    public void testColorier() {
+
+    }
+
+    @Test
+    public void testCreateEnSVG() {
+
+    }
+
+    @Test
+    public void testDeplacer() {
+
+    }
+
+    @Test
+    public void testDescription() {
+
+    }
+
+    @Test
+    public void testDupliquer() {
+
+    }
+
+    @Test
+    public void testEnSVG() {
+
+    }
+
+    @Test
+    public void testGetCouleur() {
+
+    }
+
+    @Test
+    public void testGetLegence() {
+
+    }
+
+    @Test
+    public void testGetNbreDePart() {
+
+    }
+
+    @Test
+    public void testGetTitre() {
+
+    }
+
+    @Test
+    public void testHauteur() {
+
+    }
+
+    @Test
+    public void testLargeur() {
+
+    }
+
+    @Test
+    public void testLegender() {
+
+    }
+
+    @Test
+    public void testRedimensionner() {
+
+    }
+
+    @Test
+    public void testSetLegence() {
+
+    }
+
+    @Test
+    public void testSetNbreDePart() {
+
+    }
+
+    @Test
+    public void testSetOptions() {
+
+    }
+
+    @Test
+    public void testSetTitre() {
+
+    }
+
+    @Test
+    public void testTourner() {
+
+    }
+}

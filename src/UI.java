381b7f66e14493bccf124eee7f9fadec76c56b84
Wojtecki Benjamin <benjamin.wojtecki@etudiant.univ-rennes1>
2024-04-02 12:46:13 +0200

    Ajout de la page web quand on clique sur le boutton

--- src/UI.java
+++ src/UI.java
@@ -12,8 +12,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
-import java.awt.MouseInfo;
-import java.awt.PointerInfo;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;

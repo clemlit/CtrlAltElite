2879d17a1982e0991a43eee471ea3f6f5fc6fc33
Amine <not.chaomare@gmail.com>
2024-04-11 17:47:09 +0200

    Région s'affiche omg?

--- src/Map.java
+++ src/Map.java
@@ -29,19 +29,15 @@ public class Map {
         if (ui != null) {
             return;
         }
-        int etendue = 1;
-String urlstring="https://img.freepik.com/premium-vector/france-map-background-with-states-france-map-isolated-white-background-vector-illustration_1003415-27.jpg";        URL url = new URL(urlstring);
-
-        if (etendue == 0) {
-            urlstring = "xd";
-        }
+        String urlstring = "https://img.freepik.com/premium-vector/france-map-background-with-states-france-map-isolated-white-background-vector-illustration_1003415-27.jpg";
+        URL url = new URL(urlstring);
         image = ImageIO.read(url);
         area = getOutline(Color.WHITE, image, 12);
         shapeList = separateShapeIntoRegions(area);
         ui = new JPanel(new BorderLayout(4, 4));
         ui.setBorder(new EmptyBorder(4, 4, 4, 4));
-
         output.addMouseListener(new MouseClickListener());
+        output.addMouseMotionListener(new MouseListen());
         output.addMouseMotionListener(new MouseClickListener());
         ui.add(output);
         refresh();
@@ -54,7 +50,6 @@ String urlstring="https://img.freepik.com/premium-vector/france-map-background-w
     public Area getOutline(Color target, BufferedImage bi, int tolerance) {
         // construct the GeneralPath
         GeneralPath gp = new GeneralPath();
-
         boolean cont = false;
         for (int xx = 0; xx < bi.getWidth(); xx++) {
             for (int yy = 0; yy < bi.getHeight(); yy++) {
@@ -84,7 +79,6 @@ String urlstring="https://img.freepik.com/premium-vector/france-map-background-w
 
     public static ArrayList<Shape> separateShapeIntoRegions(Shape shape) {
         ArrayList<Shape> regions = new ArrayList<>();
-
         PathIterator pi = shape.getPathIterator(null);
         GeneralPath gp = new GeneralPath();
         while (!pi.isDone()) {
@@ -120,57 +114,79 @@ String urlstring="https://img.freepik.com/premium-vector/france-map-background-w
 
     class MouseListen implements MouseMotionListener {
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        // Code à exécuter lorsque la souris est déplacée en maintenant un bouton enfoncé
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            // Code à exécuter lorsque la souris est déplacée en maintenant un bouton
+            // enfoncé
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+
+            int width = output.getWidth();
+            int height = output.getHeight();
+
+            if (x >= 0 && x < width && y >= 0 && y < height) {
+                refresh();
+            } else {
+            }
+        }
     }
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        // Code à exécuter lorsque la souris est déplacée sans maintenir de bouton enfoncé
+    class MouseClickListener implements MouseMotionListener, MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX(); // Coordonnée X du clic de la souris
+            int y = e.getY(); // Coordonnée Y du clic de la souris
+            int i = 0;
+            for (Shape s : shapeList) {
+
+                if (s.contains(x, y)) {
+                    System.out.print(i);
+                    if(i==22){
+                        System.out.print("Bretagne");
+                    }
+                }
+                i++;
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // Code à exécuter lorsque le bouton de la souris est enfoncé
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // Code à exécuter lorsque le bouton de la souris est relâché
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // Code à exécuter lorsque la souris entre dans la zone du composant
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // Code à exécuter lorsque la souris quitte la zone du composant
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            // Code à exécuter lorsque la souris est déplacée en maintenant un bouton
+            // enfoncé
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // Code à exécuter lorsque la souris est déplacée sans maintenir de bouton
+            // enfoncé
+        }
+
     }
-}
-
-class MouseClickListener implements MouseMotionListener, MouseListener {
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("get out of china");
-        // Code à exécuter lors du clic de la souris
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // Code à exécuter lorsque le bouton de la souris est enfoncé
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // Code à exécuter lorsque le bouton de la souris est relâché
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // Code à exécuter lorsque la souris entre dans la zone du composant
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // Code à exécuter lorsque la souris quitte la zone du composant
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        // Code à exécuter lorsque la souris est déplacée en maintenant un bouton enfoncé
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        // Code à exécuter lorsque la souris est déplacée sans maintenir de bouton enfoncé
-    }
-}
-
-
 
     public static boolean isIncluded(Color target, Color pixel, int tolerance) {
         int rT = target.getRed();

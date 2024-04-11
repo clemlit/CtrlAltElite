b6cecaa69386283f60c8312eef3f75d5f545de5e
Sami Saoud <sami.saoud@etudiant.univ-rennes1.fr>
2024-04-11 17:40:16 +0200

    AJout map dans UI

--- src/UI.java
+++ src/UI.java
@@ -21,6 +21,7 @@ public class UI implements ActionListener {
     JPanel panelFiltres;
     JPanel mapPanel;
     JButton themeButton;
+    Map map;
 
     public static void main(String argv[]) {
         new UI();
@@ -69,59 +70,59 @@ public class UI implements ActionListener {
         multiBox<String> comboCarbu = new multiBox<>();
         multiBox<String> comboOptions = new multiBox<>();
 
-        String[] filtres = { "Prix moyen", "Prix médian  ", "Prix minimum  ",
-                "Nombre de stations qui proposent chaque type de carburant  ",
-                "Nombre de stations qui proposent des services spécifiques  " };
+        String[] filtres = { "Prix moyen", "Prix médian ", "Prix minimum ",
+                "Nombre de stations qui proposent chaque type de carburant ",
+                "Nombre de stations qui proposent des services spécifiques " };
 
         for (String filtre : filtres) {
             comboFiltres.addItem(filtre);
         }
 
         String[] regions = {
-                "Auvergne-Rhône-Alpes  ", "Bourgogne-Franche-Comté  ", "Bretagne  ", "Centre-Val de Loire  ", "Corse  ",
-                "Grand Est  ", "Hauts-de-France  ", "Île-de-France  ", "Normandie  ", "Nouvelle-Aquitaine  ",
-                "Occitanie  ", "Pays de la Loire  ", "Provence-Alpes-Côte d'Azur"
+                "Auvergne-Rhône-Alpes ", "Bourgogne-Franche-Comté ", "Bretagne ", "Centre-Val de Loire ", "Corse ",
+                "Grand Est ", "Hauts-de-France ", "Île-de-France ", "Normandie ", "Nouvelle-Aquitaine ",
+                "Occitanie ", "Pays de la Loire ", "Provence-Alpes-Côte d'Azur"
         };
 
         for (String region : regions) {
             comboRegion.addItem(region);
         }
         String[] departements = {
-                "01 - Ain  ", "02 - Aisne  ", "03 - Allier  ", "04 - Alpes-de-Haute-Provence  ", "05 - Hautes-Alpes  ",
-                "06 - Alpes-Maritimes  ", "07 - Ardèche  ", "08 - Ardennes  ", "09 - Ariège  ", "10 - Aube  ",
-                "11 - Aude  ", "12 - Aveyron  ", "13 - Bouches-du-Rhône  ", "14 - Calvados  ", "15 - Cantal  ",
-                "16 - Charente  ", "17 - Charente-Maritime  ", "18 - Cher  ", "19 - Corrèze  ", "20 - Corse  ",
-                "21 - Côte-d'Or  ", "22 - Côtes-d'Armor  ", "23 - Creuse  ", "24 - Dordogne  ", "25 - Doubs  ",
-                "26 - Drôme  ", "27 - Eure  ", "28 - Eure-et-Loir  ", "29 - Finistère  ", "2A - Corse-du-Sud  ",
-                "2B - Haute-Corse  ", "30 - Gard  ", "31 - Haute-Garonne  ", "32 - Gers  ", "33 - Gironde  ",
-                "34 - Hérault  ", "35 - Ille-et-Vilaine  ", "36 - Indre  ", "37 - Indre-et-Loire  ", "38 - Isère  ",
-                "39 - Jura  ", "40 - Landes  ", "41 - Loir-et-Cher  ", "42 - Loire  ", "43 - Haute-Loire  ",
-                "44 - Loire-Atlantique  ", "45 - Loiret  ", "46 - Lot  ", "47 - Lot-et-Garonne  ", "48 - Lozère  ",
-                "49 - Maine-et-Loire  ", "50 - Manche  ", "51 - Marne  ", "52 - Haute-Marne  ", "53 - Mayenne  ",
-                "54 - Meurthe-et-Moselle  ", "55 - Meuse  ", "56 - Morbihan  ", "57 - Moselle  ", "58 - Nièvre  ",
-                "59 - Nord  ", "60 - Oise  ", "61 - Orne  ", "62 - Pas-de-Calais  ", "63 - Puy-de-Dôme  ",
-                "64 - Pyrénées-Atlantiques  ", "65 - Hautes-Pyrénées  ", "66 - Pyrénées-Orientales  ",
-                "67 - Bas-Rhin  ", "68 - Haut-Rhin  ", "69 - Rhône  ", "70 - Haute-Saône  ", "71 - Saône-et-Loire  ",
-                "72 - Sarthe  ", "73 - Savoie  ", "74 - Haute-Savoie  ", "75 - Paris  ", "76 - Seine-Maritime  ",
-                "77 - Seine-et-Marne  ", "78 - Yvelines  ", "79 - Deux-Sèvres  ", "80 - Somme  ", "81 - Tarn  ",
-                "82 - Tarn-et-Garonne  ", "83 - Var  ", "84 - Vaucluse  ", "85 - Vendée  ", "86 - Vienne  ",
-                "87 - Haute-Vienne  ", "88 - Vosges  ", "89 - Yonne  ", "90 - Territoire de Belfort  ",
-                "91 - Essonne  ", "92 - Hauts-de-Seine  ", "93 - Seine-Saint-Denis  ", "94 - Val-de-Marne  ",
-                "95 - Val-d'Oise  "
+                "01 - Ain ", "02 - Aisne ", "03 - Allier ", "04 - Alpes-de-Haute-Provence ", "05 - Hautes-Alpes ",
+                "06 - Alpes-Maritimes ", "07 - Ardèche ", "08 - Ardennes ", "09 - Ariège ", "10 - Aube ",
+                "11 - Aude ", "12 - Aveyron ", "13 - Bouches-du-Rhône ", "14 - Calvados ", "15 - Cantal ",
+                "16 - Charente ", "17 - Charente-Maritime ", "18 - Cher ", "19 - Corrèze ", "20 - Corse ",
+                "21 - Côte-d'Or ", "22 - Côtes-d'Armor ", "23 - Creuse ", "24 - Dordogne ", "25 - Doubs ",
+                "26 - Drôme ", "27 - Eure ", "28 - Eure-et-Loir ", "29 - Finistère ", "2A - Corse-du-Sud ",
+                "2B - Haute-Corse ", "30 - Gard ", "31 - Haute-Garonne ", "32 - Gers ", "33 - Gironde ",
+                "34 - Hérault ", "35 - Ille-et-Vilaine ", "36 - Indre ", "37 - Indre-et-Loire ", "38 - Isère ",
+                "39 - Jura ", "40 - Landes ", "41 - Loir-et-Cher ", "42 - Loire ", "43 - Haute-Loire ",
+                "44 - Loire-Atlantique ", "45 - Loiret ", "46 - Lot ", "47 - Lot-et-Garonne ", "48 - Lozère ",
+                "49 - Maine-et-Loire ", "50 - Manche ", "51 - Marne ", "52 - Haute-Marne ", "53 - Mayenne ",
+                "54 - Meurthe-et-Moselle ", "55 - Meuse ", "56 - Morbihan ", "57 - Moselle ", "58 - Nièvre ",
+                "59 - Nord ", "60 - Oise ", "61 - Orne ", "62 - Pas-de-Calais ", "63 - Puy-de-Dôme ",
+                "64 - Pyrénées-Atlantiques ", "65 - Hautes-Pyrénées ", "66 - Pyrénées-Orientales ",
+                "67 - Bas-Rhin ", "68 - Haut-Rhin ", "69 - Rhône ", "70 - Haute-Saône ", "71 - Saône-et-Loire ",
+                "72 - Sarthe ", "73 - Savoie ", "74 - Haute-Savoie ", "75 - Paris ", "76 - Seine-Maritime ",
+                "77 - Seine-et-Marne ", "78 - Yvelines ", "79 - Deux-Sèvres ", "80 - Somme ", "81 - Tarn ",
+                "82 - Tarn-et-Garonne ", "83 - Var ", "84 - Vaucluse ", "85 - Vendée ", "86 - Vienne ",
+                "87 - Haute-Vienne ", "88 - Vosges ", "89 - Yonne ", "90 - Territoire de Belfort ",
+                "91 - Essonne ", "92 - Hauts-de-Seine ", "93 - Seine-Saint-Denis ", "94 - Val-de-Marne ",
+                "95 - Val-d'Oise "
         };
 
         for (String departement : departements) {
             comboDepart.addItem(departement);
         }
 
-        String[] carburants = { "Gazole  ", "SP95  ", "SP98  ", "E10  ", "E85  ", "GPLc" };
+        String[] carburants = { "Gazole ", "SP95 ", "SP98 ", "E10 ", "E85 ", "GPLc" };
         for (String carburant : carburants) {
             comboCarbu.addItem(carburant);
         }
 
-        String[] options = { "Wi-fi  ", "Boutique alimentaire  ", "Station de gonflage  ", "Lavage automatique  ",
-                "Bornes éléctrique  ", "Automate CB 24/24  ",
-                "DAB (Distributeur automatique de billets)  ", "Espace bébé  ", "Toilettes publiques  " };
+        String[] options = { "Wi-fi ", "Boutique alimentaire ", "Station de gonflage ", "Lavage automatique ",
+                "Bornes éléctrique ", "Automate CB 24/24 ",
+                "DAB (Distributeur automatique de billets) ", "Espace bébé ", "Toilettes publiques " };
 
         for (String option : options) {
             comboOptions.addItem(option);
@@ -187,6 +188,9 @@ public class UI implements ActionListener {
         f.setLocationRelativeTo(null);
         f.setVisible(true);
 
+        map = new Map();
+        mapPanel.add(map.getUI());
+
         JButton boutonResultat = new JButton("Resultat");
         boutonResultat.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -202,8 +206,8 @@ public class UI implements ActionListener {
 
                 List<Object> selectedRegions = comboRegion.getSelectedItems();
                 if (!selectedRegions.isEmpty()) {
-                // Ajoute les données sélectionnées dans le multiBox "comboRegion"
-                htmlContent.append("<h2> Région selectionné :</h2>");
+                    // Ajoute les données sélectionnées dans le multiBox "comboRegion"
+                    htmlContent.append("<h2> Région selectionné :</h2>");
                 }
                 for (Object region : selectedRegions) {
                     htmlContent.append("<p>").append(region.toString()).append("</p>");
@@ -211,7 +215,7 @@ public class UI implements ActionListener {
 
                 List<Object> selectedDepartements = comboDepart.getSelectedItems();
                 if (!selectedDepartements.isEmpty()) {
-                htmlContent.append("<h2>Département selectionné :</h2>");
+                    htmlContent.append("<h2>Département selectionné :</h2>");
                 }
                 for (Object departement : selectedDepartements) {
                     // Ajoute les données sélectionnées dans le multiBox "comboDepart"
@@ -224,7 +228,7 @@ public class UI implements ActionListener {
                     htmlContent.append("<h2>Carburant selectionné : </h2>");
                 }
                 for (Object carbu : selectedCarbus) {
-                    
+
                     htmlContent.append("<p>").append(carbu.toString()).append("</p>");
                 }
 
@@ -234,7 +238,6 @@ public class UI implements ActionListener {
                     writer.write("<html><head><title>Résultats</title>");
                     writer.write("<style>");
                     writer.write("body {");
-                    writer.write("    display: flex;");
                     writer.write("    justify-content: center;"); // Centrer horizontalement
                     writer.write("    align-items: center;"); // Centrer verticalement
                     writer.write("    height: 100vh;"); // 100% de la hauteur de la vue (viewport)
